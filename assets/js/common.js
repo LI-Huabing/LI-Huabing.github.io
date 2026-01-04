4e8e5ee524807530751b8927189427e9643141df
LI-Huabing <i@lihuabing.site>
2026-01-04 13:34:49 +0100

    Updated on 04/01/2026

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-// aHR0cHM6Ly9naXRodWIuY29tL2x1b3N0MjYvYWNhZGVtaWMtaG9tZXBhZ2U=
 $(function () {
     $('.lazy').Lazy({
         scrollDirection: 'vertical',
@@ -11,7 +10,6 @@ $(function () {
         }
     })
     $('[data-toggle="tooltip"]').tooltip()
-    
     $('.news-year-toggle').click(function() {
         $(this).next('.news-year-content').toggle();
         $(this).find('i').toggleClass('fa-chevron-down fa-chevron-right');
